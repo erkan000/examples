0cf93ea43c8f82176c118d4ea054586b93d3f5b7
erkan000 <erkan000@hotmail.com>
2020-02-29 12:58:11 +0300

    Validator

--- jsf/src/main/java/example/jsf/Bean.java
+++ jsf/src/main/java/example/jsf/Bean.java
@@ -12,18 +12,36 @@ public class Bean implements Serializable{
 	 * 
 	 */
 	private static final long serialVersionUID = 1292315409067940127L;
-	private int aValue = 12;
+	private int intValue = 12;
+	private String stringValue;
+	private long longValue;
 	
 	public void test() {
 		System.out.println("Test");
 	}
 
-	public int getaValue() {
-		return aValue;
+	public String getStringValue() {
+		return stringValue;
 	}
 
-	public void setaValue(int aValue) {
-		this.aValue = aValue;
+	public void setStringValue(String stringValue) {
+		this.stringValue = stringValue;
+	}
+
+	public long getLongValue() {
+		return longValue;
+	}
+
+	public void setLongValue(long longValue) {
+		this.longValue = longValue;
+	}
+
+	public int getIntValue() {
+		return intValue;
+	}
+
+	public void setIntValue(int intValue) {
+		this.intValue = intValue;
 	}
 
 }
---
+++ jsf/src/main/java/example/jsf/validator/EmailValidator.java
@@ -0,0 +1,52 @@
+package example.jsf.validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+@FacesValidator(value = "validators.EmailValidator")
+public class EmailValidator implements Validator<Object> {
+
+	private static final String EMAIL_PATTERN = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+	private Pattern pattern;
+	private Matcher matcher;
+
+	public EmailValidator() {
+		pattern = Pattern.compile(EMAIL_PATTERN);
+	}
+
+	@Override
+	public void validate(FacesContext context, UIComponent arg1, Object value)
+			throws ValidatorException {
+		matcher = pattern.matcher(value.toString());
+		if (!matcher.matches()) {
+			FacesMessage message = new FacesMessage("E-Mail not valid.", "error");
+			message.setSeverity(FacesMessage.SEVERITY_ERROR);
+			throw new ValidatorException(message);
+		}
+	}
+
+	public Pattern getPattern() {
+		return pattern;
+	}
+
+	public void setPattern(Pattern pattern) {
+		this.pattern = pattern;
+	}
+
+	public Matcher getMatcher() {
+		return matcher;
+	}
+
+	public void setMatcher(Matcher matcher) {
+		this.matcher = matcher;
+	}
+
+}

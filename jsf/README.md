21184788e606230d73dcfd9748f24d9130b89c50
erkan000 <erkan000@hotmail.com>
2020-04-16 22:45:25 +0300

    Converted to JSF 2.3

--- jsf/README.md
+++ jsf/README.md
@@ -24,3 +24,32 @@ This is a very basic JSF Sample. It uses maven liberty plug-in
 			 -->
 			 
 javax.faces.STATE_SAVING_METHOD : View state'leri sunucu mu yoksa client mı yönetsin? client seçilir ise view expired exception doğal olarak sunucu hiçbir zaman fırlatmaz.
+
+JSF 2.3 ile ManagedBean ler deprecate oldu, View katmanında controller olarak artık CDI Beans leri kullanacağız(Named anotasyonu kullanarak) Ayrıca JSF artifacts , validators, converter, behaviours CDI @Inject kullanılarak inject edilebilir.
+
+private ExternalContext externalContext
+private SecurityContext securityContext;
+private FacesContext facesContext;
+private ServletContext servletContext
+
+#### Micro frontend
+JSF'in controller kısmından JAX-RS API nin sağladığı ClientBuilder nesnesi ile restfull servisleri çağırabiliriz. Bu şekilde frontend'i micro frontend 'e çevirebiliriz. Örn;
+
+	Client client = ClientBuilder.newClient();
+		WebTarget resource = client
+                .target("http://jsonplaceholder.typicode.com")
+                .path("users")
+                .path("1");
+        List<Pojo> rosterList = resource.request(javax.ws.rs.core.MediaType.APPLICATION_XML).get(
+                    new GenericType<List<Pojo>>() {
+            });
+            
+Form'dan gelen veriyi PUT metodu ile şöyle yazabiliriz.
+
+	Form form = new Form();
+     form.param("name", pojo.getName());
+     form.param("number", pojo.getNumber());
+            
+     Invocation.Builder invocationBuilder = resource.request(MediaType.APPLICATION_XML);
+     Response response = invocationBuilder.post(Entity.entity(form, MediaType.APPLICATION_FORM_URLENCODED_TYPE), Response.class);
+
--- jsf/src/main/java/example/jsf/ParameterBean.java
+++ jsf/src/main/java/example/jsf/ParameterBean.java
@@ -2,8 +2,9 @@ package example.jsf;
 
 import java.io.Serializable;
 import javax.enterprise.context.RequestScoped;
-import javax.faces.context.FacesContext;
+import javax.faces.context.ExternalContext;
 import javax.faces.event.ActionEvent;
+import javax.inject.Inject;
 import javax.inject.Named;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -17,13 +18,15 @@ public class ParameterBean implements Serializable{
 	 */
 	private static final long serialVersionUID = 7781585654619412685L;
 	private String param1;
+	
+	@Inject
+	private ExternalContext externalContext ;
 
 	// Getting all variables
 	// Map<String, Object> map = FacesContext.getCurrentInstance().getExternalContext().getRequestMap();
 	
 	public void method1(){
-		FacesContext context = FacesContext.getCurrentInstance();
-		HttpServletRequest request = (HttpServletRequest)context.getExternalContext().getRequest();
+		HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
 		String var1 = request.getParameter("var1");
 		HttpSession session = request.getSession();
 		session.setAttribute("var1Response", var1);
@@ -31,8 +34,7 @@ public class ParameterBean implements Serializable{
 
 	public void method2(ActionEvent e){
 		String var2 = (String)e.getComponent().getAttributes().get("var2");
-		FacesContext context = FacesContext.getCurrentInstance();
-		HttpServletRequest request = (HttpServletRequest)context.getExternalContext().getRequest();
+		HttpServletRequest request = (HttpServletRequest) externalContext.getRequest();
 		HttpSession session = request.getSession();
 		session.setAttribute("var2Response", var2);
 	}
